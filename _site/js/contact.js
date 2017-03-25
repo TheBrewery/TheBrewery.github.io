bf61cf91cd84da8d10289fb7514fae84ff107cc1
Jim Hildensperger <jhildensperger@gmail.com>
2017-03-25 15:37:14 +0100

    updates to contact form

--- _site/js/contact.js
+++ _site/js/contact.js
@@ -5,41 +5,47 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
 
-            var name = $("input#_name").val();
-            var email = $("input#_replyto").val();
-            var phone = $("input#_phone").val();
-            var message = $("textarea#_message").val();
+            const name = $("input#_name").val();
+            const email = $("input#_replyto").val();
+            const phone = $("input#_phone").val();
+            const message = $("textarea#_message").val();
 
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', 'http://formspree.io/contact@thebrewery.io', true);
-            xhr.setRequestHeader("Accept", "application/json");
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            $.ajax({
+                url: "//formspree.io/contact@thebrewery.io",
+                method: "POST",
+                data: {
+                    name: name,
+                    email: email,
+                    phone: phone,
+                    message: message
+                },
+                dataType: "json",
+                success: function() {
+                  $('#success').html("<div class='alert alert-success'>");
+                  $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                  $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
+                  $('#success > .alert-success').append('</div>');
 
-            xhr.send( "name=" + name + "&email=" + email + "&phone=" + phone + "&message=" + message);
-            xhr.onloadend = function (res) {
-              if (res.target.status === 200) {
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-                    $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success').append('</div>');
+                  setTimeout(function() {
+                      $('#success').fadeOut('fast');
+                  }, 2000);
 
-                    //clear all fields
-                    setTimeout(function() {
-                        $('#success').fadeOut('fast');
-                    }, 2000);
+                  $('#success').fadeIn('fast');
+                  $('#contactForm').trigger("reset");
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                  $('#success').html("<div class='alert alert-danger'>");
+                  $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                  $('#success > .alert-danger').append("<strong>Sorry, it seems that my mail server is not responding. Please try again later!");
+                  $('#success > .alert-danger').append('</div>');
 
-                    $('#success').fadeIn('fast');
-                    $('#contactForm').trigger("reset");
-              } else {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry, it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
+                  setTimeout(function() {
+                      $('#success').fadeOut('fast');
+                  }, 2000);
 
-                    $('#success').fadeIn('fast');
-                    //clear all fields
-              }
-            };
+                  $('#success').fadeIn('fast');
+                }
+            });
         },
         filter: function() {
             return $(this).is(":visible");
